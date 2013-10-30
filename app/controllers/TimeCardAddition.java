e200183adfa93ab6c66a5fd53156453ecf0646ac
madhukar <madhukarpandey007@gmail.com>
2013-10-30 12:20:34 +0530

    add and edit timecard

--- app/controllers/TimeCardAddition.java
+++ app/controllers/TimeCardAddition.java
@@ -20,73 +20,108 @@ import org.slf4j.LoggerFactory;
 import controllers.auth.Secure;
 import play.db.jpa.JPA;
 import play.mvc.Controller;
+import play.mvc.Scope.Session;
 import play.mvc.With;
 
 @With(Secure.class)
 public class TimeCardAddition extends Controller {
 	private static final Logger log = LoggerFactory.getLogger(TimeCardAddition.class);
 
-	public static void postTimeAddition(String date,float[] noofhours, String Description)
+	public static void postTimeAddition(Integer timeCardId,Integer[] dayCardsid, String date, float[] noofhours, String detail)
 			throws Throwable {
-		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-		LocalDate beginOfWeek = formatter.parseLocalDate(date);
 		Integer id = null;
-		UserDbo user = Utility.fetchUser();
-		CompanyDbo company = user.getCompany();
-		UserDbo manager = user.getManager();
-		DateTimeFormatter fmt = DateTimeFormat.forPattern("EEEEEEEE");
-		if (manager.getBeginDayOfWeek()!=null && manager.getBeginDayOfWeek().equalsIgnoreCase("Saturday")) {
-			beginOfWeek = beginOfWeek.minusDays(2);
-		}
-		if (manager.getBeginDayOfWeek()!=null && manager.getBeginDayOfWeek().equalsIgnoreCase("Sunday")) {
-			beginOfWeek = beginOfWeek.minusDays(1);
-		}
-		TimeCardDbo timeCardDbo = new TimeCardDbo();
-		timeCardDbo.setBeginOfWeek(beginOfWeek);
-		float totalhours = 0;
-		for (int i = 0; i < 7; i++) {
-			DayCardDbo dayC = new DayCardDbo();
-			dayC.setDate(beginOfWeek.plusDays(i));
-			dayC.setDay(fmt.print(beginOfWeek.plusDays(i)));
-			if (noofhours[i] > 12) {
-				validation.addError("noofhours[i]",
-						"hours should be less than 12");
-			} else {
-				dayC.setNumberOfHours(noofhours[i]);
-				totalhours = totalhours + noofhours[i];
-				timeCardDbo.addDayCard(dayC);
-				JPA.em().persist(dayC);
+		if (timeCardId == null || timeCardId == 0) {
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+			LocalDate beginOfWeek = formatter.parseLocalDate(date);
+			UserDbo user = Utility.fetchUser();
+			CompanyDbo company = user.getCompany();
+			UserDbo manager = user.getManager();
+			DateTimeFormatter fmt = DateTimeFormat.forPattern("EEEEEEEE");
+			if (manager.getBeginDayOfWeek() != null	&& manager.getBeginDayOfWeek().equalsIgnoreCase("Saturday")) {
+				beginOfWeek = beginOfWeek.minusDays(2);
 			}
-		}
-		if (validation.hasErrors()) {
-			params.flash(); // add http parameters to the flash scope
-			validation.keep(); // keep the errors for the next request
-			// addTime();
-		}
-		JPA.em().flush();
+			if (manager.getBeginDayOfWeek() != null	&& manager.getBeginDayOfWeek().equalsIgnoreCase("Sunday")) {
+				beginOfWeek = beginOfWeek.minusDays(1);
+			}
+			TimeCardDbo timeCardDbo = new TimeCardDbo();
+			timeCardDbo.setBeginOfWeek(beginOfWeek);
+			float totalhours = 0;
+			for (int i = 0; i < 7; i++) {
+				DayCardDbo dayC = new DayCardDbo();
+				dayC.setDate(beginOfWeek.plusDays(i));
+				dayC.setDay(fmt.print(beginOfWeek.plusDays(i)));
+				if (noofhours[i] > 12) {
+					validation.addError("noofhours[i]",
+							"hours should be less than 12");
+				} else {
+					dayC.setNumberOfHours(noofhours[i]);
+					totalhours = totalhours + noofhours[i];
+					timeCardDbo.addDayCard(dayC);
+					JPA.em().persist(dayC);
+				}
+			}
+			if (validation.hasErrors()) {
+				params.flash(); // add http parameters to the flash scope
+				validation.keep(); // keep the errors for the next request
+			}
+			JPA.em().flush();
 
-		timeCardDbo.setNumberOfHours(totalhours);
-		timeCardDbo.setDetail(Description);
-		timeCardDbo.setApproved(false);
-		timeCardDbo.setStatus(StatusEnum.SUBMIT);
-		user.addTimecards(timeCardDbo);
-		JPA.em().persist(timeCardDbo);
-		JPA.em().persist(user);
-		String key = Utility.generateKey();
-		SecureToken secureToken = new SecureToken();
-		secureToken.setSecureToken(key);
-		secureToken.setValue(timeCardDbo.getId());
-		JPA.em().persist(secureToken);
-		JPA.em().flush();
-		if (manager.isGetEmailYesOrNo() !=null && manager.isGetEmailYesOrNo().equalsIgnoreCase("yes")) {
-			Utility.sendEmailForApproval(manager.getEmail(), company.getName(),	user.getEmail(),key);
+			timeCardDbo.setNumberOfHours(totalhours);
+			timeCardDbo.setDetail(detail);
+			timeCardDbo.setApproved(false);
+			timeCardDbo.setStatus(StatusEnum.SUBMIT);
+			user.addTimecards(timeCardDbo);
+			JPA.em().persist(timeCardDbo);
+			JPA.em().persist(user);
+			String key = Utility.generateKey();
+			SecureToken secureToken = new SecureToken();
+			secureToken.setSecureToken(key);
+			secureToken.setValue(timeCardDbo.getId());
+			JPA.em().persist(secureToken);
+			JPA.em().flush();
+			if (manager.isGetEmailYesOrNo() != null	&& manager.isGetEmailYesOrNo().equalsIgnoreCase("yes")) {
+				Utility.sendEmailForApproval(manager.getEmail(),company.getName(), user.getEmail(), key);
+			}
+			OtherStuff.home(id);
+		} else {
+			TimeCardDbo timeCard = JPA.em().find(TimeCardDbo.class, timeCardId);
+			int sum = 0;
+			for (int i = 0; i < 7; i++) {
+				DayCardDbo dayC = JPA.em().find(DayCardDbo.class, dayCardsid[i]);
+				if (noofhours[i] > 12) {
+					validation.addError("noofhours[i]",
+							"hours should be less than 12");
+				} else {
+					dayC.setNumberOfHours(noofhours[i]);
+					JPA.em().persist(dayC);
+					sum += noofhours[i];
+				}
+				if (validation.hasErrors()) {
+					params.flash();
+					validation.keep();
+					OtherStuff.home(id);
+				}
+
+			}
+			timeCard.setNumberOfHours(sum);
+			timeCard.setStatus(StatusEnum.SUBMIT);
+			timeCard.setDetail(detail);
+			JPA.em().persist(timeCard);
+			JPA.em().flush();
+			OtherStuff.home(id);
+
 		}
-		OtherStuff.home(id);
 	}
 
-	public static void addEditTimeCardRender(String date) {
-	DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-		LocalDate beginOfWeek = formatter.parseLocalDate(date);
+	public static void addEditTimeCardRender(String date,int id) {
+		LocalDate beginOfWeek=null;
+		if (!(id == 0)) {
+			TimeCardDbo timeCard = JPA.em().find(TimeCardDbo.class, id);
+			beginOfWeek = timeCard.getBeginOfWeek();
+			render(timeCard, beginOfWeek);
+		} else {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+		beginOfWeek = formatter.parseLocalDate(date);
 		UserDbo user = Utility.fetchUser();
 		UserDbo manager = user.getManager();
 		TimeCardDbo timeCard = null;
@@ -107,6 +142,23 @@ public class TimeCardAddition extends Controller {
 				dayC.setDay(fmt.print(beginOfWeek.plusDays(i)));
 			}
 		render(timeCard, beginOfWeek);
+		}
+	}
+
+	public static void deleteTimeCardRender(Integer timeCardId) {
+		Integer id = timeCardId;
+		render(id);
+	}
+
+	public static void postDeleteTimeCard(Integer timeCardId) {
+		Integer id = null;
+		String email = Session.current().get("username");
+		UserDbo user = UserDbo.findByEmailId(JPA.em(), email);
+		TimeCardDbo timeCard = JPA.em().find(TimeCardDbo.class, timeCardId);
+		user.deleteTimeCard(timeCard);
+		JPA.em().persist(user);
+		JPA.em().flush();
+		OtherStuff.home(id);
 	}
 	public static void timeCardApproval(String key){
 		SecureToken token =JPA.em().find(SecureToken.class, key);
